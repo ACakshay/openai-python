12b91675c531fa2ca67a44adb71c9ea0ebd8eafe
dhrsingh1 <dhruv.singhal@publicissapient.com>
2024-05-03 14:03:04 +0530

    straming fix

--- src/openai/_streaming.py
+++ src/openai/_streaming.py
@@ -56,11 +56,15 @@ class Stream(Generic[_T]):
         iterator = self._iter_events()
 
         for sse in iterator:
-            if sse.data.startswith("[DONE]"):
+            if sse.data.startswith('["DONE"]'):
                 break
-
+ 
             if sse.event is None:
-                data = sse.json()
+                try:
+                    data = sse.json()
+                except json.JSONDecodeError:
+                    continue
+                
                 if is_mapping(data) and data.get("error"):
                     message = None
                     error = data.get("error")
